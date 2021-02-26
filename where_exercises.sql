37e3ebf5f3d16c0f2c98a2fe9ecbd83b5a40e570
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-02-26 09:33:55 -0600

    .

--- where_exercises.sql
+++ where_exercises.sql
@@ -18,6 +18,6 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
-SELECT * FROM employees WHERE hire_date LIKE '%-12-25' AND birth_date LIKE '199%';
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
